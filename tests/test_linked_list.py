042c4d31b7e351cfe9cb9ffa4a2bfb047775e8b3
alexandr50 <dmitrymaslov2016@mail.ru>
2023-03-28 00:37:55 +0300

    Добавил методы to_list, get_data_by_id в класс LinkedList, написал к ним тесты

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -23,3 +23,23 @@ class Test(unittest.TestCase):
     def test_print_empty(self):
         linked_list = LinkedList()
         self.assertEqual(str(linked_list), 'None')
+
+
+    def test_to_list(self):
+        linked_list = LinkedList()
+        linked_list.insert_beginning({2: 2})
+        linked_list.insert_beginning({1: 1})
+        linked_list.insert_at_end({3: 3})
+        linked_list.insert_at_end({4: 4})
+        self.assertEqual(linked_list.to_list(), [{1: 1}, {2: 2}, {3: 3}, {4: 4}])
+
+
+    def test_get_data_by_id(self):
+        linked_list = LinkedList()
+        linked_list.insert_beginning([1, 1])
+        self.assertEqual(linked_list.get_data_by_id(1), None)
+        linked_list.insert_beginning({'id': 2})
+        # linked_list.insert_beginning({1: 1})
+        # linked_list.insert_at_end({3: 3})
+        # linked_list.insert_at_end({4: 4})
+        self.assertEqual(linked_list.get_data_by_id(2), {'id': 2})
